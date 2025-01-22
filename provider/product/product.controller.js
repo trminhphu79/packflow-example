bafc1ef07e6af03a114f0225db4116fbf5c4754b
Tran Minh Phu <trminhphu79@gmail.com>
2025-01-22 23:35:08 +0700

    update log

--- provider/product/product.controller.js
+++ provider/product/product.controller.js
@@ -3,11 +3,17 @@ const ProductRepository = require("./product.repository");
 const repository = new ProductRepository();
 
 exports.getAll = async (req, res) => {
-    res.send(await repository.fetchAll())
+  console.log("Execute get all product...");
+  res.send(await repository.fetchAll());
+  console.log("Finished get all product...");
 };
 exports.getById = async (req, res) => {
-    const product = await repository.getById(req.params.id);
-    product ? res.send(product) : res.status(404).send({message: "Product not found"})
+  console.log("Execute get product by id " + req.params.id);
+  const product = await repository.getById(req.params.id);
+  console.log("Finished get product by id " + req.params.id);
+  product
+    ? res.send(product)
+    : res.status(404).send({ message: "Product not found" });
 };
 
 exports.repository = repository;
--- provider/product/product.repository.js
+++ provider/product/product.repository.js
@@ -1,22 +1,27 @@
-const Product = require('./product');
+const Product = require("./product");
 
 class ProductRepository {
+  constructor() {
+    this.products = new Map([
+      ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1")],
+      ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")],
+      ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2")],
+      [
+        "12",
+        new Product("12", "MICHAEL_TRAN", "Hello bro, I'm Michael :))", "v3"),
+      ],
+      ["13", new Product("13", "DON_HO", "Hello bro, I'm Don HO :))", "v3")],
+      ["14", new Product("14", "HIP", "Hello bro, I'm HIP :))", "v3")],
+    ]);
+  }
 
-    constructor() {
-        this.products = new Map([
-            ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1")],
-            ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")],
-            ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2")],
-        ]);
-    }
+  async fetchAll() {
+    return [...this.products.values()];
+  }
 
-    async fetchAll() {
-        return [...this.products.values()]
-    }
-
-    async getById(id) {
-        return this.products.get(id);
-    }
+  async getById(id) {
+    return this.products.get(id);
+  }
 }
 
 module.exports = ProductRepository;
